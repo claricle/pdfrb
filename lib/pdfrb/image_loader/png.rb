80236a9d1020ceb2885c31327724c30c230e7b2a
Ronald Tse <ronald.tse@ribose.com>
2026-08-04 16:18:39 +0800

    Fix image loader API (.call) and transformation matrix test

--- lib/pdfrb/image_loader/png.rb
+++ lib/pdfrb/image_loader/png.rb
@@ -5,11 +5,12 @@ module Pdfrb
     module PNG
       module_function
 
-      def load(document, path_or_io)
-        data = read_data(path_or_io)
-        info = parse_png_header(data)
+      def call(document, data, **_opts)
+        data = data.read if data.is_a?(IO) || data.is_a?(StringIO)
+        info = parse_header(data)
+        return nil if info.empty?
 
-        stream = document.add(
+        image = document.add(
           {
             Type: :XObject,
             Subtype: :Image,
@@ -18,48 +19,33 @@ module Pdfrb
             ColorSpace: info[:color_space],
             BitsPerComponent: info[:bits],
             Filter: :FlateDecode,
-            DecodeParms: {
-              Predictor: 15,
-              Columns: info[:width],
-            },
+            DecodeParms: { Predictor: 15, Columns: info[:width] },
             Length: 0,
           },
-          type: Pdfrb::Model::Cos::Stream
+          type: Pdfrb::Model::Type::XObjectImage
         )
-        stream.stream = ""
-        stream
+        image.stream = ""
+        image
       end
 
-      def parse_png_header(data)
-        return {} unless data && data.bytesize >= 24
-
+      def parse_header(data)
+        return {} unless data && data.is_a?(String) && data.bytesize >= 24
         return {} unless data.start_with?("\x89PNG\r\n\x1A\n".b)
-
-        chunk_len = (data.getbyte(8) << 24) | (data.getbyte(9) << 16) | (data.getbyte(10) << 8) | data.getbyte(11)
         return {} unless data.bytes[12, 4].pack("C*") == "IHDR"
 
-        offset = 16
-        width = read_u32(data, offset)
-        height = read_u32(data, offset + 4)
-        bits = data.getbyte(offset + 8)
-        color_type = data.getbyte(offset + 9)
-
-        color_space = case color_type
-                      when 0, 4 then :DeviceGray
-                      when 2, 6 then :DeviceRGB
-                      when 3 then :Indexed
-                      else :DeviceRGB
-                      end
-
-        { width: width, height: height, bits: bits, color_space: color_space, color_type: color_type }
-      end
-
-      def read_data(path_or_io)
-        case path_or_io
-        when String then File.binread(path_or_io)
-        when IO, StringIO then path_or_io.read
-        else path_or_io.to_s
-        end
+        off = 16
+        {
+          width: read_u32(data, off),
+          height: read_u32(data, off + 4),
+          bits: data.getbyte(off + 8),
+          color_type: data.getbyte(off + 9),
+          color_space: case data.getbyte(off + 9)
+                       when 0, 4 then :DeviceGray
+                       when 2, 6 then :DeviceRGB
+                       when 3 then :Indexed
+                       else :DeviceRGB
+                       end,
+        }
       end
 
       def read_u32(data, off)
