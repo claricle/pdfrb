5348398d32fd6b7c5bf944f00ee72ea27da24d31
Ronald Tse <ronald.tse@ribose.com>
2026-08-04 17:13:34 +0800

    Fix empty annotation type files (write proper class definitions)

---
+++ lib/pdfrb/model/type/text_annotation.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Pdfrb
+  module Model
+    module Type
+      class TextAnnotation < Annotation
+        def content; self[:Contents]; end
+        def popup; self[:Popup]; end
+        def color; self[:C]; end
+      end
+    end
+  end
+end
