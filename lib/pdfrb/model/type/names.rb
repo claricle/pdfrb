68d71a267a974a7ad7b392e8279510aaf460bb87
Ronald Tse <ronald.tse@ribose.com>
2026-08-05 08:47:48 +0800

    Deepen 7 more type classes: OutputIntent, Names, XObjectImage, XObjectForm, FileTrailer, Info, ObjectStream, XRefStream
    - OutputIntent: pdfx?/pdfa? detection, profile resolution, ICC fields.
    - Names: each_embedded_file name-tree walker (handles /Names and /Kids
      recursively), has_javascript?/has_destinations? predicates.
    - XObjectImage: width/height/color_space/bits_per_component accessors,
      components lookup by color space, masked?/has_soft_mask?/
      inline_soft_mask?/indexed?/bytes_per_row helpers.
    - XObjectForm: bbox/matrix/group/transparency_group?/isolated?/
      knockout?/identity_matrix? helpers.
    - FileTrailer: encrypted?/incremental?/has_id?/original_id/current_id/
      resolved_root/info/encrypt helpers.
    - Info: title/author/subject/keywords/creator/producer accessors,
      creation_time/modification_time parse PDF date format.
    - ObjectStream: number_of_objects/first_byte_offset, each_object_reference
      parses the OID/offset index from the decoded stream.
    - XRefStream: field_widths/subsection_index parse the W and Index
      arrays used for cross-reference decoding.

--- lib/pdfrb/model/type/names.rb
+++ lib/pdfrb/model/type/names.rb
@@ -8,6 +8,53 @@ module Pdfrb
       # /EmbeddedFiles, /AlternatePresentations, /Renditions.
       class Names < Pdfrb::Model::Cos::Dictionary
         arlington_object "Name"
+
+        def destinations; self[:Dests]; end
+        def ap; self[:AP]; end
+        def javascript; self[:JavaScript]; end
+        def pages; self[:Pages]; end
+        def templates; self[:Templates]; end
+        def urls; self[:URLS]; end
+        def embedded_files; self[:EmbeddedFiles]; end
+        def alternate_presentations; self[:AlternatePresentations]; end
+        def renditions; self[:Renditions]; end
+
+        def has_embedded_files?
+          !!embedded_files
+        end
+
+        def has_javascript?
+          !!javascript
+        end
+
+        def has_destinations?
+          !!destinations
+        end
+
+        def each_embedded_file
+          return enum_for(:each_embedded_file) unless block_given?
+          return unless embedded_files && document
+
+          walk_name_tree(embedded_files) { |name, ref| yield name, ref }
+        end
+
+        private
+
+        def walk_name_tree(node_ref, &block)
+          node = node_ref.is_a?(Pdfrb::Model::Reference) && document ?
+                   document.object(node_ref) : node_ref
+          return unless node
+
+          if node[:Names]
+            arr = node[:Names]
+            arr = arr.is_a?(Pdfrb::Model::PdfArray) ? arr.to_a : arr
+            arr.each_slice(2) { |name, ref| yield name, ref }
+          elsif node[:Kids]
+            arr = node[:Kids]
+            arr = arr.is_a?(Pdfrb::Model::PdfArray) ? arr.to_a : arr
+            arr.each { |kid| walk_name_tree(kid, &block) }
+          end
+        end
       end
     end
   end
--- lib/pdfrb/model/type/object_stream.rb
+++ lib/pdfrb/model/type/object_stream.rb
@@ -8,6 +8,42 @@ module Pdfrb
       class ObjectStream < Pdfrb::Model::Cos::Stream
         arlington_object "ObjectStream"
         register_type :ObjStm
+
+        def type; self[:Type]; end
+        def number_of_objects; self[:N]; end
+        def first_byte_offset; self[:First]; end
+        def extends; self[:Extends]; end
+        def filter; self[:Filter]; end
+
+        def has_parent?
+          !!extends
+        end
+
+        def compressed_count
+          number_of_objects || 0
+        end
+
+        def each_object_reference
+          return enum_for(:each_object_reference) unless block_given?
+          return unless number_of_objects && first_byte_offset
+
+          stream_data = decoded_stream
+          return unless stream_data
+
+          offset = 0
+          number_of_objects.times do
+            space_idx = stream_data.index(" ", offset)
+            return if space_idx.nil?
+            oid = stream_data[offset...space_idx].to_i
+
+            newline_idx = stream_data.index(/\s/, space_idx + 1)
+            return if newline_idx.nil?
+            byte_offset = stream_data[space_idx + 1...newline_idx].to_i
+
+            yield oid, byte_offset + first_byte_offset
+            offset = newline_idx + 1
+          end
+        end
       end
     end
   end
