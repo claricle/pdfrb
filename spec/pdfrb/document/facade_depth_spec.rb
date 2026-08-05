105d06b6cc065dbdc71c628e489c15d30e095cda
Ronald Tse <ronald.tse@ribose.com>
2026-08-05 13:06:01 +0800

    Add 11 more depth specs covering EncryptionPublicKey, DSS, OCConfig, PageLabel, BorderStyling, BorderEffect, ViewerPreferences, ActionGoTo, ActionURI

--- spec/pdfrb/document/facade_depth_spec.rb
+++ spec/pdfrb/document/facade_depth_spec.rb
@@ -96,3 +96,114 @@ RSpec.describe Pdfrb::Encryption::SecurityHandler, ".registry" do
     expect(described_class.lookup("AdobePubSec")).to be_nil
   end
 end
+
+RSpec.describe "Model::Type::* additional depth coverage" do
+  let(:doc) { Pdfrb::Document.new }
+
+  describe Pdfrb::Model::Type::EncryptionPublicKey do
+    it "exposes public-key parameters" do
+      enc = doc.add({ Filter: :AdobePubSec, V: 4, Length: 128,
+                      Recipients: [{}, {}, {}] },
+                    type: described_class)
+      expect(enc.public_key?).to be true
+      expect(enc.aes?).to be true
+      expect(enc.key_length_bytes).to eq(16)
+      expect(enc.recipient_count).to eq(3)
+    end
+  end
+
+  describe Pdfrb::Model::Type::DocumentSecurityStore do
+    it "counts certs/ocsp/crl entries" do
+      dss = doc.add({ Type: :DSS, Certs: [{}, {}, {}], OCSPs: [{}] },
+                    type: described_class)
+      expect(dss.cert_count).to eq(3)
+      expect(dss.ocsp_count).to eq(1)
+      expect(dss.crl_count).to eq(0)
+      expect(dss.has_vri?).to be false
+    end
+  end
+
+  describe Pdfrb::Model::Type::OptionalContentConfiguration do
+    it "decodes base state and intent" do
+      config = doc.add({ BaseState: :OFF, Intent: [:View, :Print] },
+                       type: described_class)
+      expect(config.all_off?).to be true
+      expect(config.view_intent?).to be true
+      expect(config.print_intent?).to be true
+    end
+  end
+
+  describe Pdfrb::Model::Type::PageLabel do
+    it "renders decimal page labels with prefix" do
+      label = doc.add({ S: :D, P: "Page ", St: 1 },
+                      type: described_class)
+      expect(label.label_for(0)).to eq("Page 1")
+      expect(label.label_for(4)).to eq("Page 5")
+    end
+
+    it "renders uppercase roman numerals" do
+      label = doc.add({ S: :R }, type: described_class)
+      expect(label.label_for(0)).to eq("I")
+      expect(label.label_for(2)).to eq("III")
+      expect(label.label_for(4)).to eq("V")
+    end
+
+    it "renders lowercase letters" do
+      label = doc.add({ S: :a }, type: described_class)
+      expect(label.label_for(0)).to eq("a")
+      expect(label.label_for(25)).to eq("z")
+      expect(label.label_for(26)).to eq("aa")
+    end
+  end
+
+  describe Pdfrb::Model::Type::BorderStyling do
+    it "decodes border style and dash" do
+      bs = doc.add({ S: :D, W: 2.5, D: [[3, 2], 0] },
+                   type: described_class)
+      expect(bs.dashed?).to be true
+      expect(bs.solid?).to be false
+      expect(bs.width).to eq(2.5)
+      expect(bs.dash_array).to eq([3, 2])
+      expect(bs.dash_phase).to eq(0)
+    end
+  end
+
+  describe Pdfrb::Model::Type::BorderEffect do
+    it "detects cloudy border" do
+      be = doc.add({ S: :C, I: 2 }, type: described_class)
+      expect(be.cloudy?).to be true
+      expect(be.inset?).to be false
+      expect(be.intensity).to eq(2)
+    end
+  end
+
+  describe Pdfrb::Model::Type::ViewerPreferences do
+    it "exposes direction and duplex predicates" do
+      vp = doc.add({ Direction: :R2L, Duplex: :DuplexFlipLongEdge },
+                   type: described_class)
+      expect(vp.right_to_left?).to be true
+      expect(vp.duplex_flip_long_edge?).to be true
+      expect(vp.simplex?).to be false
+    end
+  end
+
+  describe Pdfrb::Model::Type::ActionGoTo do
+    it "decodes page-index destinations" do
+      action = doc.add({ Type: :Action, S: :GoTo, D: [3, :Fit] },
+                       type: described_class)
+      expect(action.page_index_destination?).to be true
+      expect(action.target_page_number).to eq(3)
+      expect(action.display_option).to eq([:Fit])
+    end
+  end
+
+  describe Pdfrb::Model::Type::ActionURI do
+    it "extracts URL scheme" do
+      action = doc.add({ Type: :Action, S: :URI, URI: "https://example.com" },
+                       type: described_class)
+      expect(action.scheme).to eq("https")
+      expect(action.http?).to be true
+      expect(action.mailto?).to be false
+    end
+  end
+end
