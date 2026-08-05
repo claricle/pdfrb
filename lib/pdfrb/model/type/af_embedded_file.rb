bbb2f2c47db3508c84d045a3cd8e35b1d4cc86d8
Ronald Tse <ronald.tse@ribose.com>
2026-08-05 09:06:49 +0800

    Deepen AF (Associated File) types per PDF 2.0 App Note 002
    - AFFileSpecification: adds /AFRelationship accessors with typed
      predicates for each ISO 32000-2 relationship (source?/data?/
      alternative?/supplement?/encrypted?/form_fillin?/schema?/
      unspecified?).
    - AFEmbeddedFile: looks up /AFRelationship via /Params dict
      (where it lives per the app note).

--- lib/pdfrb/model/type/af_embedded_file.rb
+++ lib/pdfrb/model/type/af_embedded_file.rb
@@ -6,6 +6,16 @@ module Pdfrb
       # PDF 2.0 Associated-File embedded stream (App Note 002).
       class AFEmbeddedFile < EmbeddedFile
         arlington_object "AFEmbeddedFileStream"
+
+        def af_relationship
+          return nil unless params
+          obj = params.is_a?(Pdfrb::Model::Reference) && document ? document.object(params) : params
+          obj && obj[:AFRelationship]
+        end
+
+        def associated_file_relationship?
+          !!af_relationship
+        end
       end
     end
   end
