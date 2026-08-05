1da0a00fd6500184b0f967d9dbea5d9a2d31da7c
Ronald Tse <ronald.tse@ribose.com>
2026-08-05 09:06:49 +0800

    Deepen AF (Associated File) types per PDF 2.0 App Note 002
    - AFFileSpecification: adds /AFRelationship accessors with typed
      predicates for each ISO 32000-2 relationship (source?/data?/
      alternative?/supplement?/encrypted?/form_fillin?/schema?/
      unspecified?).
    - AFEmbeddedFile: looks up /AFRelationship via /Params dict
      (where it lives per the app note).

--- lib/pdfrb/model/type/af_file_specification.rb
+++ lib/pdfrb/model/type/af_file_specification.rb
@@ -7,6 +7,40 @@ module Pdfrb
       # /AFRelationship to the base FileSpec.
       class AFFileSpecification < FileSpecification
         arlington_object "AFFileSpecification"
+
+        def af_relationship; self[:AFRelationship]; end
+
+        def source?
+          af_relationship&.to_sym == :Source
+        end
+
+        def data?
+          af_relationship&.to_sym == :Data
+        end
+
+        def alternative?
+          af_relationship&.to_sym == :Alternative
+        end
+
+        def supplement?
+          af_relationship&.to_sym == :Supplement
+        end
+
+        def encrypted?
+          af_relationship&.to_sym == :EncryptedPayload
+        end
+
+        def form_fillin?
+          af_relationship&.to_sym == :FormData
+        end
+
+        def schema?
+          af_relationship&.to_sym == :Schema
+        end
+
+        def unspecified?
+          af_relationship.nil? || af_relationship&.to_sym == :Unspecified
+        end
       end
     end
   end
